04964302bbc7f2d89943be615521ba2f4e12a102
Camilo4201 <cruizb@unal.edu.co>
2024-11-20 20:11:31 -0500

    Delete reto_01.2.py

--- reto_01.2.py
+++
@@ -1,4 +0,0 @@
-#Realice una función que permita validar si una palabra es un palíndromo. Condición: 
-# No se vale hacer slicing para invertir la palabra y verificar que sea igual a la original.
-
-print(range(10))
